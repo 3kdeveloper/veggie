83f7624ecb6697fa96a13bd16dd19e6600638881
kamran khan <77343380+3kdeveloper@users.noreply.github.com>
2022-03-23 14:50:23 +0500

    organizing code structure

---
+++ lib/view/splash/splash_screen.dart
@@ -0,0 +1,10 @@
+import 'package:veggie/utils/exports.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
